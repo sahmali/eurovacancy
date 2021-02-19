111cc4e30caa1cdc5bd6b7de39a6a2ddf09fb9fd
sahmali <sahmali.hesenov@gmail.com>
2021-02-20 02:40:45 +0400

    home page is ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ serviceItem.hover(function(){
 });
 
 
-$('.owl-carousel').owlCarousel({
+$('#our-team-section .owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:false,
@@ -22,4 +22,57 @@ $('.owl-carousel').owlCarousel({
             items:3
         }
     }
+});
+$('#slider-section .owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    dots:false,
+    autoPlay:true,
+    navClass:["slider-prev" , "slider-next"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+$('#brands-section .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+
+
+  AOS.init();
+
+
+///navbar
+
+const vegan = document.querySelector(".vegan");
+const navList = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+vegan.addEventListener('click',()=>{
+    navList.classList.toggle('open');
+    links.forEach(link=>{
+        link.classList.toggle('fade');
+    })
 })
